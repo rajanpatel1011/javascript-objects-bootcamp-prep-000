a58439c468ececfb97cb58fed783c2efe936a8c1
Ryan Patel <rajanpatel1011@gmail.com>
2018-03-15 16:28:26 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -8,3 +8,6 @@ function updatePlaylist(playlist,artistName,songTitle){
 return Object.assign({},playlist,{[artistName]:songTitle})
  
 }
+function removeFromPlaylist(playlist,artistName){
+  delete playlist.artistName;
+}
